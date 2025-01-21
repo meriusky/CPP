9af90417079d98632399d3951a613f14b4590b58
Meritxell <meriusky@Ordenador-portatil-de-Meritxell.local>
2025-01-21 13:18:16 +0100

    ex00✅

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+#include <iostream>  //Llibreria per  std::cout, std::cin
+#include <cctype>    //Llibreria per std::toupper
+
+void UpperCase(char* str)
+{
+	int i = 0;
+    while (str[i] != '\0') 
+	{
+        str[i] = std::toupper(str[i]); //funcio que transforma en majuscula
+		i++;
+    }
+}
+
+int main(int argc, char* argv[]) 
+{
+	int i = 1;
+    if (argc == 2) 
+	{
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return 1;
+    }
+	else if(argc > 2)
+	{
+		while(argv[i] != NULL)
+		{
+			UpperCase(argv[i]);
+			std::cout  << argv[i];
+			i++;
+		}
+		if(argv[i] == NULL)
+			std::cout << std::endl;
+		return 0;
+	}
+}
+

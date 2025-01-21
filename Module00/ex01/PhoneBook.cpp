1ba3e14325002eb3b92223c19a07258aa874ebfb
Meritxell <meriusky@Ordenador-portatil-de-Meritxell.local>
2025-01-21 19:11:05 +0100

    caos with ex01🪫

---
+++ Module00/ex01/PhoneBook.cpp
@@ -0,0 +1,11 @@
+#include "PhoneBook.hpp"
+
+class PhoneBook
+{
+	private:
+		string first_name;
+		string last_name;
+		string nickname;
+		string darkest_secret;
+}
+
---
+++ Module00/ex01/PhoneBook.hpp
@@ -0,0 +1,23 @@
+#include <iostream> // std::cout, std::cin
+#include <string> //para hacer cosas con trings, la verdad ns
+
+class PhoneBook
+{
+	private:
+		string first_name;
+		string last_name;
+		string nickname;
+		string darkest_secret;	
+}
+class Contacts
+{
+	private:
+		string first;
+		string second;
+		string third;
+		string fourth;
+		string fifth;
+		string sixth;
+		string seventh;
+		string eighth;
+}

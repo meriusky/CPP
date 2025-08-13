3c7e6df4efda79102cc89288b14caa2c37c381b2
Meritxell <meriusky@Ordenador-portatil-de-Meritxell.local>
2025-08-13 10:36:22 +0200

    ex00 ✅

--- Module05/ex00/main.cpp
+++ Module05/ex00/main.cpp
@@ -37,45 +37,49 @@
 #include <iostream>
 #include "Bureaucrat.hpp"
 
-int main() {
-    std::cout << "=== Creating valid bureaucrats ===" << std::endl;
-    Bureaucrat bob("Bob", 40);
-    Bureaucrat alice("Alice", 2);
-
-    std::cout << bob << std::endl;
-    std::cout << alice << std::endl;
-
-    std::cout << "\n=== Testing grade increment/decrement ===" << std::endl;
-    std::cout << "before" << bob << std::endl;
-    bob.incrementGrade();
-    alice.incrementGrade();
-    std::cout << "\n increment Grade" << std::endl;
-    std::cout << bob << std::endl;
-    std::cout << alice << std::endl;
-    bob.decrementGrade();
-    alice.decrementGrade();
-    std::cout << "\n decrement Grade" << std::endl;
-    std::cout << bob << std::endl;
-    std::cout << alice << std::endl;
-
-    std::cout << "\n=== Creating invalid bureaucrats ===" << std::endl;
-    try 
+int main() 
+{
+    try
     {
-        Bureaucrat tooHigh("High", 0); // Invalid
-    } 
-    catch (const std::exception& e) 
-    {
-        std::cerr << e.what() << std::endl;
+        std::cout << "=== Creating bureaucrats ===" << std::endl;
+        Bureaucrat bob("Bob", 2);
+        Bureaucrat alice("Alice", 140);
+
+    
+        std::cout << bob << std::endl;
+        std::cout << alice << std::endl;
+
+        std::cout << "=== TESTING GRADE ⬆️increment/decrement⬇️ ===" << std::endl;
+    // std::cout << "before" << bob << std::endl;
+        bob.incrementGrade();
+        alice.incrementGrade();
+        std::cout << "\n ⬆️increment Grade⬆️" << std::endl;
+        std::cout << bob << std::endl;
+        std::cout << alice << std::endl;
+        bob.decrementGrade();
+        alice.decrementGrade();
+        std::cout << "\n ⬇️decrement Grade⬇️" << std::endl;
+        std::cout << bob << std::endl;
+        std::cout << alice << std::endl;
     }
-    try 
-    {
-        Bureaucrat tooLow("Low", 151); // Invalid
-    } 
-    //For weird errors
+    // std::cout << "\n=== Creating invalid bureaucrats ===" << std::endl;
+    // try 
+    // {
+    //     Bureaucrat tooHigh("High", 0); // Invalid
+    // } 
     catch (const std::exception& e) 
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    {                                //📌What is std::cerr?
+        std::cerr << e.what() << std::endl; //Is the "cousin" of std::cout but for errors and ist
+    }                                       //unbuffered(prints immediately), thats why is good for                                      
+    // try                                  //errors. It prints faster than the crush 
+    // {                                     
+    //     Bureaucrat tooLow("Low", 151); // Invalid
+    // } 
+    // //For weird errors
+    // catch (const std::exception& e) 
+    // {
+    //     std::cerr << e.what() << std::endl;
+   // }
 
     return 0;
 }
